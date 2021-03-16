bbc536fedc4d778ba3f4f1470e7cd094f65afccf
Mel Grubb <melgrubb@live.com>
2021-03-16 15:48:42 -0400

    Implemented suggested changes

---
+++ src/Publish-Local.ps1
@@ -0,0 +1,21 @@
+Write-Output "Synchronizing Assembly Version Info"
+[xml]$buildProps = Get-Content BuilderGenerator\BuilderGenerator.csproj
+$version = $buildProps.Project.PropertyGroup.Version
+$buildProps.Project.PropertyGroup.AssemblyVersion = $version
+$buildProps.Project.PropertyGroup.FileVersion = $version
+$buildProps.Save((Resolve-Path "BuilderGenerator\BuilderGenerator.csproj"))
+
+Write-Output "Synchronizing Package Version Info"
+[xml]$packageProps = Get-Content BuilderGenerator.Package\BuilderGenerator.Package.csproj
+$packageProps.Project.PropertyGroup[1].PackageVersion = $version
+$packageProps.Save((Resolve-Path "BuilderGenerator.Package\BuilderGenerator.Package.csproj"))
+
+#Write-Output "Building version $version"
+#dotnet build .\BuilderGenerator\BuilderGenerator.csproj --configuration release --verbosity minimal
+
+Write-Output "Building Package Version $version"
+dotnet build .\BuilderGenerator.Package\BuilderGenerator.Package.csproj --configuration release --verbosity minimal
+
+$path = '.\BuilderGenerator.Package\bin\Release\BuilderGenerator.' + $version + '.nupkg'
+Write-Output "Publishing package '$path'"
+dotnet nuget push $path --source 'C:\Projects\NuGet Local Repo'
