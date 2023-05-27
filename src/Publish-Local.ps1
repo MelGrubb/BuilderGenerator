dae01499722db3fc734f06a0d03525e832c34897
Mel Grubb <MelGrubb@live.com>
2023-05-27 11:11:54 -0400

    Updated local publish script

--- src/Publish-Local.ps1
+++ src/Publish-Local.ps1
@@ -1,21 +1,7 @@
-Write-Output "Synchronizing Assembly Version Info"
 [xml]$buildProps = Get-Content BuilderGenerator\BuilderGenerator.csproj
 $version = $buildProps.Project.PropertyGroup.Version
-$buildProps.Project.PropertyGroup.AssemblyVersion = $version
-$buildProps.Project.PropertyGroup.FileVersion = $version
-$buildProps.Save((Resolve-Path "BuilderGenerator\BuilderGenerator.csproj"))
+$version = "$version".Trim()
 
-Write-Output "Synchronizing Package Version Info"
-[xml]$packageProps = Get-Content BuilderGenerator.Package\BuilderGenerator.Package.csproj
-$packageProps.Project.PropertyGroup[1].PackageVersion = $version
-$packageProps.Save((Resolve-Path "BuilderGenerator.Package\BuilderGenerator.Package.csproj"))
-
-#Write-Output "Building version $version"
-#dotnet build .\BuilderGenerator\BuilderGenerator.csproj --configuration debug --verbosity minimal
-
-Write-Output "Building Package Version $version"
-dotnet build .\BuilderGenerator.Package\BuilderGenerator.Package.csproj --configuration debug --verbosity minimal
-
-Write-Output "Publishing package to local NuGet repo"
-$path = '.\BuilderGenerator.Package\bin\Debug\BuilderGenerator.' + $version + '.nupkg'
-dotnet nuget push $path --source 'C:\Projects\NuGet Local Repo'
+Write-Output "Publishing package version '$version' to local NuGet repo"
+$path = '.\BuilderGenerator\bin\Debug\BuilderGenerator.' + $version + '.nupkg'
+dotnet nuget push $path --source 'C:\NuGet\BuilderGenerator'
