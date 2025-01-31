2249308a0df8b0c9b32589c1b22f5b80d034f016
Mel Grubb <melgrubb@live.com>
2025-01-31 09:23:20 -0500

    Removed .net 6 and 7 integration tests

--- src/BuilderGenerator.Tests.Integration.Net60/Builders/CollectionTypeSampleBuilder.cs
+++
@@ -1,8 +0,0 @@
-using BuilderGenerator.Tests.Core.Models.Entities;
-
-namespace BuilderGenerator.Tests.Integration.Net60.Builders;
-
-[BuilderFor(typeof(CollectionTypesSample))]
-public partial class CollectionTypeSampleBuilder
-{
-}
