a2d2121bce24c6ee45341f078d66dbfdf4219184
Mel Grubb <melgrubb@live.com>
2025-01-31 09:18:04 -0500

    45-ignore-obsolete-properties (#50)
    * #45 Complete
    * Updated pipeline builds to v8

--- src/BuilderGenerator.Tests.Unit/Examples/InputWithoutInternals.cs
+++ src/BuilderGenerator.Tests.Unit/Examples/InputWithoutInternals.cs
@@ -1,4 +1,5 @@
 using BuilderGenerator;
+using System;
 
 namespace BuilderGenerator.UnitTests
 {
@@ -7,9 +8,12 @@ namespace BuilderGenerator.UnitTests
         public string FirstName { get; set; }
         public string LastName { get; set; }
         internal string InternalString { get; set; }
+
+        [Obsolete]
+        public string MiddleName { get; set; }
     }
 
-    [BuilderFor(typeof(Person), false)]
+    [BuilderFor(typeof(Person), false, false)]
     public partial class PersonBuilderWithoutInternals
     {
     }
--- src/Publish-Local.ps1
+++ src/Publish-Local.ps1
@@ -1,25 +1,26 @@
-[xml]$buildProps = Get-Content BuilderGenerator\BuilderGenerator.csproj
+[xml]$buildProps = Get-Content Directory.Build.props
 $version = $buildProps.Project.PropertyGroup.Version
+$packageVersion = $buildProps.Project.PropertyGroup.PackageVersion
 
-Write-Output "Uninstalling package"
-Uninstall-Package -Id BuilderGenerator -ProjectName BuilderGenerator.Sample.NuGet
+#Write-Output "Uninstalling package"
+#Uninstall-Package -Id BuilderGenerator -ProjectName BuilderGenerator.Sample.NuGet
 
-Write-Output "Deleting local package cache version $version"
-if (test-path %userprofile%\.nuget\packages\buildergenerator\$version)
+Write-Output "Deleting local package cache version $packageVersion"
+if (test-path %userprofile%\.nuget\packages\buildergenerator\$packageVersion)
 {
-    rm %userprofile%\.nuget\packages\buildergenerator\$version -recurse
+    rm %userprofile%\.nuget\packages\buildergenerator\$packageVersion -recurse
 }
 
-$path = "C:\Projects\NuGet Local Repo\BuilderGenerator.$version.nupkg"
+$path = "C:\Projects\NuGet Local Repo\BuilderGenerator.$packageVersion.nupkg"
 Write-Output "Deleting local NuGet package '$path'"
 if (test-path $path)
 {
     rm $path
 }
 
-Write-Output "Publishing package version '$version' to local NuGet repo"
-$path = '.\BuilderGenerator\bin\Debug\BuilderGenerator.' + $version + '.nupkg'
-dotnet nuget push $path --source 'C:\NuGet\BuilderGenerator'
+Write-Output "Publishing package version '$packageVersion' to local NuGet repo"
+$path = '.\BuilderGenerator\bin\Debug\BuilderGenerator.' + $packageVersion + '.nupkg'
+dotnet nuget push $path --source 'C:\Projects\NuGet Local Repo\BuilderGenerator'
 
-Write-Output "Installing new package version"
-Install-Package -Id BuilderGenerator -ProjectName BuilderGenerator.Sample.NuGet
+#Write-Output "Installing new package version"
+#Install-Package -Id BuilderGenerator -ProjectName BuilderGenerator.Sample.NuGet
